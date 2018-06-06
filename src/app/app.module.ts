a556682247d617c966f7291baa281a8e6de68a94
ledomone <ledom.git@gmail.com>
2018-06-06 21:20:46 +0200

    Adding ProductService As A Provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,9 @@ import { ProductService } from './product.service';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    ProductService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
